d369572d105bb95de6e8329db70dcfa2dc5c8b21
unknown <you@example.com>
2021-11-23 10:16:32 +0700

    Initial commit

--- socialMedia/wsgi.py
+++ socialMedia/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "socialMedia.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mxh2.settings")
 
 application = get_wsgi_application()
