8e61b7f697aa52b996fdf9f88f3b32a1594496a5
unknown <you@example.com>
2021-11-23 10:42:18 +0700

    Initial commit

--- socialMedia/settings.py
+++ socialMedia/settings.py
@@ -16,8 +16,9 @@ import django_heroku
 import dj_database_url
 DATABASES = { 'default': dj_database_url.config() }
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
