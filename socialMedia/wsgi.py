82f6877c083766811e9620a99c6183ebc3428917
unknown <you@example.com>
2021-11-23 10:20:13 +0700

    Initial commit

--- socialMedia/wsgi.py
+++ socialMedia/wsgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
+"""""
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +15,12 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mxh2.settings")
 
 application = get_wsgi_application()
+"""""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mxh2.settings")
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
