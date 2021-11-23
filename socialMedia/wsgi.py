b74287732b35bdb840c45816774ee6bbcb4163d1
unknown <you@example.com>
2021-11-23 10:44:40 +0700

    Initial commit

--- socialMedia/wsgi.py
+++ socialMedia/wsgi.py
@@ -18,7 +18,7 @@ application = get_wsgi_application()
 """""
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "socialMedia.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mxh2.settings")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
